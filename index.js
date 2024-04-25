23bb73cd01c48f19aa7698c86fa77e223128e7e2
resarisyan <resarisyan77@gmail.com>
2024-04-25 20:53:04 +0700

    feat: add write and read file

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-import { volumeBalok, luasPermukaanBalok } from './balok.js';
-import { volumeKubus, luasPermukaanKubus } from './kubus.js';
-import { volumePrisma, luasPermukaanPrisma } from './prisma.js';
+import { volumeBalok, luasPermukaanBalok } from './src/balok.js';
+import { volumeKubus, luasPermukaanKubus } from './src/kubus.js';
+import { volumePrisma, luasPermukaanPrisma } from './src/prisma.js';
+import { saveData, loadData } from './src/common/utils.js';
+// console.log(volumeBalok(2, 3, 4));
+// console.log(luasPermukaanBalok(2, 3, 4));
 
-console.log(volumeBalok(2, 3, 4));
-console.log(luasPermukaanBalok(2, 3, 4));
+// console.log(volumeKubus(3));
+// console.log(luasPermukaanKubus(3));
 
-console.log(volumeKubus(3));
-console.log(luasPermukaanKubus(3));
+// console.log(volumePrisma(2, 3, 4));
+// console.log(luasPermukaanPrisma(2, 3, 4));
 
-console.log(volumePrisma(2, 3, 4));
-console.log(luasPermukaanPrisma(2, 3, 4));
+saveData({
+  id: 11,
+  name: 'Clementina DuBuque 2',
+  username: 'Moriah.Stanton 2',
+  email: 'Rey.Padberg@karina.biz',
+});
+
+console.log(loadData());
--- src/balok.js
+++ src/balok.js
@@ -1,9 +1,13 @@
+import fs from 'fs';
+
 const volumeBalok = (panjang, lebar, tinggi) => {
   return panjang * lebar * tinggi;
 };
 
 const luasPermukaanBalok = (panjang, lebar, tinggi) => {
-  return 2 * (panjang * lebar + panjang * tinggi + lebar * tinggi);
+  const data = 2 * (panjang * lebar + panjang * tinggi + lebar * tinggi);
+  fs.writeFileSync('./data/result.txt', data.toString(), 'utf8');
+  return data;
 };
 
 export { volumeBalok, luasPermukaanBalok };
